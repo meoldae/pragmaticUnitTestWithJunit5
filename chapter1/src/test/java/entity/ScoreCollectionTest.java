0c05fb8b083fc76c23317e8f72371208f643bbdf
Junyoung <wnsdud12365@naver.com>
2025-09-29 23:29:34 +0900

    Refactor: Change to multi module project for chaptering

---
+++ chapter1/src/test/java/entity/ScoreCollectionTest.java
@@ -0,0 +1,22 @@
+package entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ScoreCollectionTest {
+
+    @Test
+    public void answerArithmeticMeanOfTwoNumbers(){
+        // Ready
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // Execute
+        int actualResult = collection.arithmeticMean();
+
+        // Compare
+        assertEquals(actualResult, 6);
+    }
+}
